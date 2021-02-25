3410128a4a10b476f269a0da24a4b701cc74309b
skrskrm <50164945+skrskrm@users.noreply.github.com>
2021-02-24 17:19:11 -0800

    Update custom-tests.js

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -105,4 +105,54 @@ describe('Party Horn Tests', () => {
       expect($el).to.have.prop('paused', false);
     });
   });
+
+  it('Test if the image and sound sources change when you select the party horn radio button', () => {
+    cy.get('#radio-party-horn').click();
+    cy.get('#sound-image').then($el => {
+      expect($el).to.have.attr('src', './assets/media/images/party-horn.svg');
+    });
+    cy.get('#horn-sound').then($el => {
+      expect($el).to.have.attr('src', './assets/media/audio/party-horn.mp3');
+    });
+  });
+
+  it('Test if the volume image changes when increasing volumes (you must test for all 3 cases)', () => {
+    cy.get('#volume-slider')
+      .invoke('val', 0)
+      .trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src', './assets/media/icons/volume-level-0.svg');
+    });
+    cy.get('#volume-slider')
+      .invoke('val', 25)
+      .trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src', './assets/media/icons/volume-level-1.svg');
+    });
+    cy.get('#volume-slider')
+      .invoke('val', 50)
+      .trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src', './assets/media/icons/volume-level-2.svg');
+    });
+    cy.get('#volume-slider')
+      .invoke('val', 75)
+      .trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src', './assets/media/icons/volume-level-3.svg');
+    });
+  });
+
+  it('Test if the honk button is disabled when the textbox input is a empty or a non-number', () => {
+    cy.get('#volume-number').clear().type(' ');
+    cy.get('#honk-btn').then($el => {
+      expect($el).to.have.attr('disabled');
+    });
+  });
+
+  it('Test if an error is shown when you type a number outside of the given range for the volume textbox input', () => {
+    cy.get('#volume-number').clear().type('120');
+    cy.get('#volume-number').then($el => $el[0].checkValidity()).should('be.false')
+  });
+
 });
